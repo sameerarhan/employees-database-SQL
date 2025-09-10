48c29cabfb06b39f6a5de702731cd4601b10398f
sameerarhan <73331454+sameerarhan@users.noreply.github.com>
2025-09-10 22:41:10 +0530

    Update 03_basic_queries.sql

--- 03_basic_queries.sql
+++ 03_basic_queries.sql
@@ -1,3 +1,7 @@
 DESCRIBE employees;
 SELECT * FROM employees;
 SELECT COUNT(*) AS No_of_rows_employees FROM employees;
+
+DESCRIBE departments;
+SELECT * FROM departments;
+SELECT COUNT(*) AS No_of_rows FROM departments;
