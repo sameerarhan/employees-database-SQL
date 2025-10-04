4fa4c2d8f05f91a096f6c8c0f85d8ab2c912ed0b
Sameer Arhan <sameerarhan9405@gmail.com>
2025-10-04 09:36:41 +0000

    Update query `besantsql`

--- 3088297-besantsql.sql
+++ 3088297-besantsql.sql
@@ -1985,7 +1985,7 @@ JOIN deparments dep
 ON dep.department_id = emp.department_id
 JOIN departments mgr_dep
 ON mgr_dep.department_id = mgr.department_id
-AND emp.salary>(SELECT salary FROM emp WHERE fi)
+AND emp.salary>(SELECT salary FROM emp WHERE first_name='Bruce')
 
 
 -- 22. Write a query to display employee name, manager's name, and their location IDs if employee working in department number 10 or 30 and manager earning more than Hazel and employee working in location ID 1700 or 1200.
