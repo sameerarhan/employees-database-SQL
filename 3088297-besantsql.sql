b49659f02920ea64e3ce9f5ff83ab38eb1e39c5f
Sameer Arhan <sameerarhan9405@gmail.com>
2025-10-06 05:11:50 +0000

    Update query `besantsql`

--- 3088297-besantsql.sql
+++ 3088297-besantsql.sql
@@ -1486,6 +1486,9 @@ FROM
 (SELECT * FROM employees))
 WHERE sl_no IN (1,3,5,8);
 
+
+FROM 
+
 -- 3(b) Query to display employee name and hire_Date of records at 1st, 3rd, 5th, and 8th positions using ROW_NUMBER() Function :
 SELECT first_name,hire_date
 FROM(
